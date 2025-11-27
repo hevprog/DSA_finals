e450724499ee10211fefe9a20c634a2fee43885b
Hen-ops <ndevacc@gmail.com>
2025-11-28 00:34:40 +0800

    Added core functionalities

--- ui/Insertion_ui.py
+++ ui/Insertion_ui.py
@@ -4,5 +4,6 @@ class Hanoi_ui(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        ttk.Label(self, text="This is hanoi").pack()
+        ttk.Label(self, text="This is the Insertion sort").pack()
         ttk.Button(self,text="back to menu").pack()
+    
