f316c758820a04e5f8a7700472cb88ba16e91b9a
Your Name <jrbinghoy2006@gmail.com>
2025-11-29 18:23:18 +0800

    added new values

--- logic/stack.py
+++ logic/stack.py
@@ -1,5 +1,56 @@
 #stack 
+import random
 
 class stack:
     def __init__(self):
-        pass
+        self.element = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.stack = []
+        self.random = random.sample(self.element, 5)
+        self.stack.append(self.random)
+
+    def showStack(self):
+        print(self.stack)
+    
+    def push(self):
+        self.random = random.sample(self.element, 1)
+        if(len(self.stack) < 7):
+            self.stack.append(self.random)
+        else:
+            print("STACK IS FULL")
+            
+    def pop(self):
+        if(len(self.stack) > 0):
+            self.stack.pop()
+        else:
+            print("STACK IS EMPTY")
+            
+    def peek(self):
+        if(len(self.stack) > 0):
+            top = stack[-1]
+        else:
+            print("STACK IS EMPTY")
+    
+    def isEmpty(self):
+        isEmpty = not bool(stack)
+
+    def size(self):
+        print("Size: ",len(stack))
+            
+        
+if __name__ == "__main__":
+    s = stack()
+
+    s.push()
+    s.showStack() 
+        
+        
+        
+
+
+    
+
+        
+        
+        
+        
+        
--- ui/Stacks_ui.py
+++ ui/Stacks_ui.py
@@ -1,9 +1,69 @@
+import sys
+import os
+import random
+import time
+import winsound
 import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
-class Hanoi_ui(ttk.Frame):
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from logic.stack import stack
+
+class stack_ui(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        ttk.Label(self, text="THIS IS A STACK").pack()
-        ttk.Button(self,text="back to menu").pack()
+        ttk.Button(self,bootstyle ="danger", text="back to menu").place(x = 15, y = 15)
+        ttk.Label(self,  bootstyle="secondary", text="STACK",font=("Arial Rounded MT Bold", 40, "bold")).place(relx=0.5, rely=0.155, anchor=CENTER)
+        
+        self.buttonGroup()
+    
+    def create_button(self, master, text):
+        if text == "SIZE":
+            bootstyle = INFO
+        elif text == "EMPTY CHECK":
+            bootstyle= INFO
+        elif text == "RESET":
+            bootstyle= DANGER
+        else:
+            bootstyle = LIGHT
+        return ttk.Button(
+            master=master,
+            text=text,
+            #command=lambda x=text: self.on_button_pressed(x),
+            bootstyle=bootstyle,
+            width=14,
+            padding=10,
+        )
+        
+    def buttonGroup(self):
+        container = ttk.Frame(master=self, padding=50)
+        ttk.Frame.place(self, x=100, y=50)
+        container.pack(expand=YES, anchor='s')
+        matrix = [
+                ("PUSH", "POP", "PEEK"),
+                ("SIZE",  "EMPTY CHECK", "RESET")
+            ]
+        for i, row in enumerate(matrix):
+                container.rowconfigure(i)
+                for j, num_txt in enumerate(row):
+                    container.columnconfigure(j, weight=2)
+                    btn = self.create_button(master=container, text=num_txt)
+                    btn.grid(row=i, column=j, padx=6, pady=6)
+    
+if __name__ == "__main__":
+    window = ttk.Window(
+        themename="vapor",
+        title="Stack",
+        resizable=(False, False)
+    )
+    window.geometry("800x600")
+        
+    test_window = stack_ui(window)
+    test_window.pack(fill="both", expand=True)
+        
+    window.mainloop()
+        
+    
     
