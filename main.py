9b50268806738d73d87ccb028a29ee45dd4a99e6
Hen-ops <ndevacc@gmail.com>
2025-11-30 07:31:43 +0800

    minor fix

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ if __name__ == "__main__":
     window.is_clicked_ToH().configure(command=switchHanoi)
     window.is_clicked_Count().configure(command=switchCounting)
     window.is_clicked_Insert().configure(command=switchInsert)
+    window.is_clicked_Binary().configure(command=switchBinary)
     window.is_clicked_quit().configure(command=window.quit)
     window.mainloop()
 
