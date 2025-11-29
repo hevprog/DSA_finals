f35f5aebc18b4fc2245ba769857226d3d9d2f69d
Nico Timothy <142752571+NicoTimothy11@users.noreply.github.com>
2025-11-29 15:11:00 +0800

    Added mew changes

--- logic/insertion.py
+++ logic/insertion.py
@@ -1,5 +1,30 @@
 #insertion sort 
 
 class insertion:
-    def __init__(self):
-        pass
+    sorted = False
+    def __init__(self, a):
+        self.arr = a
+    
+    def sort(self):
+        self.sorted = True
+        key = 0, n = 10
+
+        for i in range(len(n)):
+            key = self.arr[i]
+            j = i - 1
+            while j >= 0 and self.arr[j] > key:
+                self.arr[j + 1] = self.arr[j];
+                j-=1
+            self.arr[j + 1] = key;
+    
+    def restart(self, a):
+        self.arr = a
+    
+    def getArray(self):
+        return self.arr if sorted else None
+
+if __name__ == "__main__":
+    unsortedArray = [63, 22, 12, 23, 45, 34, 11, 23, 78, 99]
+    insertion = insertion(unsortedArray)
+    insertion.sort()
+    print(insertion.getArray())
