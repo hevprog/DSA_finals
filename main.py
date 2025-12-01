a90528497091e1acbeda58974c0cfa7e9156c1b4
Zyxary <kelpselp@gmail.com>
2025-12-01 16:18:17 +0800

    Main-ui: Fixed for title not displaying properly?

--- main.py
+++ main.py
@@ -21,34 +21,35 @@ class frame_ops:
 class Main_window(ttk.Window,frame_ops):
     def __init__(self):
         super().__init__(themename="vapor") # puydi kita mag darkly or cyborg na theme, kamo bahala
-        self.title("DSA Sim")
-        self.attributes('-fullscreen', True)
-
+        self.title("DSA Simulator")
+        self.geometry("1000x600") # or mas duro dako pa like 1000x650 or 1200×700?
+        self.resizable(False, False)
         self.frame = ttk.Frame(master=self)
         self.current_frame = self.get_frame() #get the current active frame
         self.frame.pack()
         self.custom_style = ttk.Style()
-        self.custom_style.configure('primary.TButton', font=('Arial', 25))
-        self.custom_style.configure('danger.TButton', font=('Arial', 25))
+        self.custom_style.configure('primary.TButton', font=('Arial', 15))
+        self.custom_style.configure('danger.TButton', font=('Arial', 15))
 
         self.widgets()
 
     def widgets(self):
-        self.label1 = ttk.Label(self, text="Data Structures and Algorithms Simulator", font=("Arial", 40, "bold")).place(relx=0.5, rely=0.1, anchor="center")
+        self.label1 = ttk.Label(self.frame, text="Data Structures and Algorithms Simulator", font=("Arial", 25, "bold"))
 
-        self.button1 = ttk.Button(self, text="Tower of Hanoi", bootstyle="primary", width=25, padding=10)
-        self.button2 = ttk.Button(self, text="Counting sort", bootstyle="primary", width=25, padding=10)
-        self.button3 = ttk.Button(self, text="Insertion sort", bootstyle="primary", width=25, padding=10)
-        self.button4 = ttk.Button(self, text="Stacks", bootstyle="primary", width=25, padding=10)
-        self.button5 = ttk.Button(self, text="Binary search", bootstyle="primary", width=25, padding=10)
-        self.button6 = ttk.Button(self, text="Quit", bootstyle="Danger", width=25, padding=10)
+        self.button1 = ttk.Button(self, text="Tower of Hanoi", bootstyle="primary", width=20)
+        self.button2 = ttk.Button(self, text="Counting sort", bootstyle="primary", width=20)
+        self.button3 = ttk.Button(self, text="Insertion sort", bootstyle="primary", width=20)
+        self.button4 = ttk.Button(self, text="Stacks", bootstyle="primary", width=20)
+        self.button5 = ttk.Button(self, text="Binary search", bootstyle="primary", width=20)
+        self.button6 = ttk.Button(self, text="Quit", bootstyle="danger", width=20)
+        self.label1.pack(pady=20)
 
-        self.button1.place(relx=0.3, rely=0.3, anchor="center")
-        self.button2.place(relx=0.7, rely=0.3, anchor="center")
-        self.button3.place(relx=0.3, rely=0.5, anchor="center")
-        self.button4.place(relx=0.7, rely=0.5, anchor="center")
-        self.button5.place(relx=0.3, rely=0.7, anchor="center")
-        self.button6.place(relx=0.7, rely=0.7, anchor="center")
+        self.button1.place(x=150, y=150)
+        self.button2.place(x=550, y=150)
+        self.button3.place(x=150, y=300)
+        self.button4.place(x=550, y=300)
+        self.button5.place(x=150, y=450)
+        self.button6.place(x=550, y=450)
     
     #accessors to the clickable widgets
     def is_clicked_ToH(self):
